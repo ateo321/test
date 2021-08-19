680d2016c072e152f4e5dc88e1335e28233daf15
ateo321 <ateo321@gmail.com>
2021-08-20 00:24:06 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -49,7 +49,9 @@ function App({ Component, pageProps }) {
         <>
             <Head>
                 <title>Authen sample</title>
-
+                Access-Control-Allow-Origin: *
+                Access-Control-Allow-Origin: <origin>
+                Access-Control-Allow-Origin: null
                 {/* bootstrap css */}
                 <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
             </Head>
