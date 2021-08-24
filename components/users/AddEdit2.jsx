9f01e554874bbd9031df8b750f84a13a517facd7
ateo321 <ateo321@gmail.com>
2021-08-24 20:28:06 +0700

    edit 21

--- components/users/AddEdit2.jsx
+++ components/users/AddEdit2.jsx
@@ -9,52 +9,16 @@ import { userService, alertService } from 'services';
 
 export { AddEdit2 };
 
-function AddEdit2(props) {
-    const user = props?.user;
-    const isAddMode = !user;
-    const router = useRouter();
-    const [showPassword, setShowPassword] = useState(false);
-    
-    // form validation rules 
-    const formOptions = { resolver: yupResolver(validationSchema) };
-
-    // set default form values if user passed in props
-    if (!isAddMode) {
-        const { password, confirmPassword, ...defaultValues } = user;
-        formOptions.defaultValues = defaultValues;
-    }
-
-    // get functions to build form with useForm() hook
-    const { register, handleSubmit, reset, formState } = useForm(formOptions);
-    const { errors } = formState;
-
+function AddEdit2() {
+	
     function onSubmit(data) {
-        return isAddMode
-            ? createUser(data)
-            : updateUser(user.id, data);
+        return 0;
     }
 
-    function createUser(data) {
-        return userService.create(data)
-            .then(() => {
-                alertService.success('User added', { keepAfterRouteChange: true });
-                router.push('.');
-            })
-            .catch(alertService.error);
-    }
-
-    function updateUser(id, data) {
-        return userService.update(id, data)
-            .then(() => {
-                alertService.success('User updated', { keepAfterRouteChange: true });
-                router.push('..');
-            })
-            .catch(alertService.error);
-    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <h1>{isAddMode ? 'Add User' : 'Edit User'}</h1>            
+            <h1>{'Edit User'}</h1>            
         </form>
     );
 }
