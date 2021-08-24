220e6eb36d631115da03230b920a08d105563f53
ateo321 <ateo321@gmail.com>
2021-08-24 14:45:19 +0700

    update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
-    ? 'https://cgkg2.sse.codesandbox.io/api' // development api
-    : 'https://cgkg2.sse.codesandbox.io/api'; // production api
+    ? '/api' // development api
+    : '/api'; // production api
 
 export {
     apiUrl
