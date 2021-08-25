f355e90aa6dd27412609b8064e22dbe05d14c73a
ateo321 <ateo321@gmail.com>
2021-08-25 13:13:51 +0700

    update id file

--- pages/users/edit/[id].jsx
+++ pages/users/edit/[id].jsx
@@ -1,7 +1,7 @@
-import { AddEdit2 } from 'components/users/AddEdit2';
+import { AddEdit } from 'components/users';
 import { userService } from 'services';
 
-export default AddEdit2;
+export default AddEdit;
 
 export async function getServerSideProps({ params }) {
     const user = await {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":1,"dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
