f20ee51891ea782d10c96b0069dd6b35752b45fd
ateo321 <ateo321@gmail.com>
2021-08-25 11:00:35 +0700

    edit 22

--- pages/users/edit/[id].jsx
+++ pages/users/edit/[id].jsx
@@ -3,4 +3,10 @@ import { userService } from 'services';
 
 export default AddEdit2;
 
+export async function getServerSideProps({ params }) {
+    const user = await userService.getById(params.id);
 
+    return {
+        props: { user }
+    }
+}
