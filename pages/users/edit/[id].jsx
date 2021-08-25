bd6ab7d8da75880ebc9d1e6078b36f0f0d07e2e6
ateo321 <ateo321@gmail.com>
2021-08-25 13:54:07 +0700

    no async

--- pages/users/edit/[id].jsx
+++ pages/users/edit/[id].jsx
@@ -5,7 +5,7 @@ export default AddEdit;
 
 export async function getServerSideProps({ params }) {
 	const test = {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":"${params.id}","dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
-    const user = await userService.getById({params.id});
+    const user = await userService.getById(params.id);
 
     return {
         props: { test }
