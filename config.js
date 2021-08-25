e73dda3ad68591a72f95d5a6db12ba4a9920104b
ateo321 <ateo321@gmail.com>
2021-08-25 16:38:09 +0700

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000/api' // development api
-    : 'https://wwww.batdongsan360.com/api'; // production api
+    : 'https://www.batdongsan360.com/api'; // production api
 
 export {
     apiUrl
