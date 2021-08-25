a60e879a0b5abb78b131900b50b73f44558da968
ateo321 <ateo321@gmail.com>
2021-08-25 13:01:29 +0700

    edit 22

--- pages/users/edit/[id].jsx
+++ pages/users/edit/[id].jsx
@@ -3,8 +3,8 @@ import { userService } from 'services';
 
 export default AddEdit2;
 
-export function getServerSideProps({ params }) {
-    const user = {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":1,"dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
+export function asycn getServerSideProps({ params }) {
+    const user = await {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":1,"dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
 
     return {
         props: { user }
