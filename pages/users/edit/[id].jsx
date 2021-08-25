4e0f4b89f4da933110dd2ef160d1954576fcc842
ateo321 <ateo321@gmail.com>
2021-08-25 14:18:13 +0700

    no async

--- pages/users/edit/[id].jsx
+++ pages/users/edit/[id].jsx
@@ -3,7 +3,7 @@ import { AddEdit } from 'components/users';
 export default AddEdit;
 
 export async function getServerSideProps({ params }) {
-	const test = {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":"${params.id}","dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
+	const test = {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":1,"dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};
     const user = await userService.getById(1);
 
     return {
--- services/user.service.js
+++ services/user.service.js
@@ -16,7 +16,8 @@ function getAll() {
 }
 
 function getById(id) {
-    return fetchWrapper.get(`${baseUrl}/${id}`);
+    return {"title":"Mr","firstName":"Frank","lastName":"Murphy","email":"frank.murphy@rustvale.com","role":"User","password":"sue123","id":1,"dateCreated":"2021-04-08T05:33:05.184Z","dateUpdated":"2021-04-15T07:20:22.768Z"};;
+	//fetchWrapper.get(`${baseUrl}/${id}`);
 }
 
 function create(params) {
