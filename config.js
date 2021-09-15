ac5e013506c3ec4949e26248df16852db89c5c2d
ateo321 <ateo321@gmail.com>
2021-09-15 13:53:40 +0700

    delte

--- config.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:3000/api' // development api
-    : 'https://www.batdongsan360.com/api'; // production api
-
-export {
-    apiUrl
-};
